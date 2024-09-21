48c846d8beaab7ed9580eb7edc4925ae0a15c149
rainsins <1820278582@qq.com>
2024-09-21 10:47:51 +0800

    update instantpage

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240921.093556';
+const cacheName = 'chirpy-20240921.104716';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
