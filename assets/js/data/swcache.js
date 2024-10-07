c5bc0d748d24f4801e54bc93680002fd59a14849
rainsins <1820278582@qq.com>
2024-10-07 14:32:20 +0800

    update marx3

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -139,7 +139,7 @@ const resource = [
 const allowedDomains = [
     
 
-    'localhost:4000',
+    'blog.rainsin.cn',
 
     
         'www.rainsin.cn',
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241007.140634';
+const cacheName = 'chirpy-20241007.143214';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
