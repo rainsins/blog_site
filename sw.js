b59870bd77afe272d050609706aab1658ff5f641
rainsins <1820278582@qq.com>
2024-10-14 20:57:36 +0800

    update umami

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241014.204545';
+const cacheName = 'chirpy-20241014.205704';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
