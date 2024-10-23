f4bc1b9676e1cb57ceb6edc4dae4d61877e2bae3
rainsins <1820278582@qq.com>
2024-10-23 13:49:49 +0800

    update certbot

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241022.143115';
+const cacheName = 'chirpy-20241023.134934';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
