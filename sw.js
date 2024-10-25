57caa879949e1be7fdb10299a202b733d5f5f85d
rainsins <1820278582@qq.com>
2024-10-25 17:35:49 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241025.162557';
+const cacheName = 'chirpy-20241025.173537';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
