75d52f41afd880c9e0d9f75b22b55038a0bd4a81
rainsins <1820278582@qq.com>
2024-10-27 13:44:21 +0800

    update font

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241027.133914';
+const cacheName = 'chirpy-20241027.134409';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
