95d0022564ebc11506cec76cff56c4880e92ee7d
rainsins <1820278582@qq.com>
2024-11-05 14:35:11 +0800

    update about:音乐和厂商的背景图片

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241105.140109';
+const cacheName = 'chirpy-20241105.143504';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
