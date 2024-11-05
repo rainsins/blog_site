f80c0e9a66be0b7b975228b2fc731565a071254d
rainsins <1820278582@qq.com>
2024-11-05 12:06:32 +0800

    update remove->marxx:删除多余的字

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241105.120228';
+const cacheName = 'chirpy-20241105.120623';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
