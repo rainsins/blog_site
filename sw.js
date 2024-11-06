0442f3c9c478b65aee0e9201be120d95dc5fdd34
rainsins <1820278582@qq.com>
2024-11-06 13:09:04 +0800

    update add->cube:修改文章的时间

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241105.143504';
+const cacheName = 'chirpy-20241106.130837';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
