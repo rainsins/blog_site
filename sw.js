64a573689741ca61659e816d2a6651c4bc67de35
rainsins <1820278582@qq.com>
2024-11-07 21:32:45 +0800

    update change->head:修改评论后台

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241106.225427';
+const cacheName = 'chirpy-20241107.213239';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
