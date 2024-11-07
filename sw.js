128d5ebd965ab98abdf273221543a89c45994f70
rainsins <1820278582@qq.com>
2024-11-07 21:52:49 +0800

    update change->about:修改评论标题

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241107.213239';
+const cacheName = 'chirpy-20241107.214053';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
