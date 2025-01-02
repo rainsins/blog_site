de948e5cb26c4b8a96b6419e93ccb82a6193b6d2
rainsins <1820278582@qq.com>
2025-01-02 22:50:48 +0800

    update theme:修改文章

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250102.224150';
+const cacheName = 'chirpy-20250102.225040';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
