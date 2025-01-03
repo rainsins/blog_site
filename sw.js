7a8e272246547cdc022c5056341379884282e45b
rainsins <1820278582@qq.com>
2025-01-03 22:16:41 +0800

    update posts:新增marx文章

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250103.162842';
+const cacheName = 'chirpy-20250103.221618';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
