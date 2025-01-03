fbaa20c911ad934818aaa188d0007e8e27050359
rainsins <1820278582@qq.com>
2025-01-03 11:16:40 +0800

    update theme:IMAGE

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250102.232941';
+const cacheName = 'chirpy-20250103.111616';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
