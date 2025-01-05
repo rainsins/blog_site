c07deb78db1d42d830687debefa88d016e9b8f73
rainsins <1820278582@qq.com>
2025-01-05 20:33:33 +0800

    update head:waline 版本

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250105.202420';
+const cacheName = 'chirpy-20250105.203258';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
