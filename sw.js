579018712e5e653ab0d7792c535f81cde5e235c7
rainsins <1820278582@qq.com>
2025-01-06 13:43:28 +0800

    update about:修改关于页面

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250106.101550';
+const cacheName = 'chirpy-20250106.134320';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
