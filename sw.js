4f24ab576fdca6d70cfc59c3eadc3ad07b16d6f3
rainsins <1820278582@qq.com>
2025-01-06 18:20:10 +0800

    update theme:字体改变

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250106.154458';
+const cacheName = 'chirpy-20250106.182004';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
