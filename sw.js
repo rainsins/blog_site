c8913c07859b875de2ddd238de8178592776e2ca
rainsins <1820278582@qq.com>
2025-01-06 15:12:56 +0800

    update main:文章卡片的描述

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250106.150502';
+const cacheName = 'chirpy-20250106.151250';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
