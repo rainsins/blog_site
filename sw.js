c9206505adb1ef9bf4ecc73b9bb6cc2b8074a65f
rainsins <1820278582@qq.com>
2025-01-06 15:05:18 +0800

    update 修改图标

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250106.145502';
+const cacheName = 'chirpy-20250106.150502';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
