90fd295da30e29dcb0d4cbb30f4fe11a519f93ea
rainsins <1820278582@qq.com>
2025-01-07 17:08:20 +0800

    update robots:增加不允许列表

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250106.222420';
+const cacheName = 'chirpy-20250107.170807';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
